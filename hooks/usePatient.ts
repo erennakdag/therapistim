e3d93dd8209ebe13a84845fb8d6f67414229cb50
Eren <erennakdag10@gmail.com>
2022-04-29 21:39:59 +0200

    changed usePatient hook, added avatar with user initials

--- hooks/usePatient.ts
+++ hooks/usePatient.ts
@@ -2,29 +2,37 @@ import { useRouter } from 'next/router';
 import { getCookie } from 'cookies-next';
 import { useEffect, useState } from 'react';
 import { fetchPatientById } from '../lib/API';
+import { IPatientData } from '../lib/types';
 
 export default () => {
+  /* 
+    We deal with the router here 
+    because this hook is guaranteed to run on the browser
+  */
   const router = useRouter();
   const id = getCookie('user');
 
-  const [data, setData] = useState<any>({});
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState({} as IPatientData);
   const [isRedirect, setRedirect] = useState(false);
 
   useEffect(() => {
     if (id === undefined) {
       router.push('/login');
       setRedirect(true);
+      setLoading(false);
       return;
     }
 
     fetchPatientById(id as string)
       .then((response) => {
         setData(response);
+        setLoading(false);
       })
       .catch((error) => {
         console.error(error);
       });
   }, []);
 
-  return [data, isRedirect];
+  return { data, isLoading, isRedirect };
 };
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -5,25 +5,33 @@ import { removeCookies } from 'cookies-next';
 import usePatient from '../../hooks/usePatient';
 
 // UI
-import { Button } from '@mantine/core';
+import { Avatar, Button, Loader } from '@mantine/core';
 
 // API
 import { deletePatientById } from '../../lib/API';
 
 export default () => {
-  const [data, isRedirect] = usePatient();
+  const { data, isLoading, isRedirect } = usePatient();
   const router = useRouter();
 
   if (isRedirect) {
     return <div>Redirecting...</div>;
+  } else if (isLoading) {
+    return <Loader />;
   }
+
+  // User/Patient's initials
+  const names = data.name.split(' ');
+  const userInitials = names[0][0] + names[names.length - 1][0];
 
   return (
     <>
       <Head>
-        <title>Profile</title>
+        <title>Profile | {data.name}</title>
       </Head>
-      <div>{JSON.stringify(data)}</div>
+      <Avatar radius='xl' size='xl'>
+        {userInitials.toUpperCase()}
+      </Avatar>
       <Button
         onClick={() => {
           removeCookies('user');
